e4a845ac0c7c925dcfe09006837825af74518edb
mgogela <m.gogela@cleanchemi.com>
2025-03-14 07:06:26 -0600

    Rough draft mapping complete
    Pre visualization mapping complete with read/write registers as listed in the data pipeline.

--- Temp/Objects/Config1/PC/Libraries.h
+++ Temp/Objects/Config1/PC/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20250313142717_
-#define _LIBRARIES_20250313142717_
+#ifndef _LIBRARIES_20250314063552_
+#define _LIBRARIES_20250314063552_
 
 __asm__(".section \".plc\"");
 
@@ -761,5 +761,5 @@ __asm__(".ascii \"iecfile \\\"Logical/modbusTCP/IDEC_Real/IDEC_Real.fun\\\" scop
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20250313142717_ */
+#endif /* _LIBRARIES_20250314063552_ */
 
